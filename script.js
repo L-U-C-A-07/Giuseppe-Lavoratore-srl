dc0026c1ac8bb7d341a8fec03b559a0de7627cb7
L-U-C-A <lucaalbertodalboni2@gmail.com>
2024-11-16 17:58:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ window.addEventListener("scroll", () => {
     let BC = document.getElementById("BlackCover");
     BC.style.opacity = Math.round((scr / 400) * 10) / 10;
 })
-
-// Import htmlComm
-// const footer = document.getElementById("footer");
-// fetch("/htmlComm/footer.html")
-// .then(res => res.text())
-// .then(data => {
-//     console.log("data:", data);
-//     footer.innerHTML = footer.innerHTML + data;
-//     console.log(footer);
-// })
