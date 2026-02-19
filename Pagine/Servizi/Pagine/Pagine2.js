a9effc774bea72fece1b284034391456abef6b69
L-U-C-A <lucaalbertodalboni2@gmail.com>
2026-02-19 17:59:51 +0100

    v 0.5.0
    major update ai servizi

--- Pagine/Servizi/Pagine/Pagine2.js
+++ Pagine/Servizi/Pagine/Pagine2.js
@@ -6,89 +6,89 @@ var Position
 var Curr
 var len
 
-function OpenImg(Sezione, Cantiere, n, img) {
-    Curr = img;
-    len = n - 1;
-    ToChange.style.backgroundImage = 'url(../../../../Images/SV/' + Sezione + '/' + Cantiere + '/' + img + '.jpg)';
-    // if (img == "l" || img == "k" || img == "m" & Sezione == "Coperture" & Cantiere == "Siracusa") {
-    //     ToChange.style.backgroundImage = 'url(../../../../Images/SV/' + Sezione + '/' + Cantiere + '/' + img + '.jpg)';
-    // }
-    // else {
-    //     ToChange.style.backgroundImage = 'url(../../../../Images/SV/' + Sezione + '/' + Cantiere + '/' + img + '.JPG)';
-    // }
-    ImgZoom.style.display = "block";
-    let h = document.querySelector("html");
-    h.style.overflowY = "hidden";
-    if (abc.indexOf(img) == 0) {
-        document.getElementById("moveL").style.opacity = ".2";
-        document.getElementById("moveL").classList.remove("Pointer");
-        document.getElementById("moveR").style.opacity = "1";
-        document.getElementById("moveR").classList.add("Pointer");
-        Position = "first";
-    }
-    else if (abc.indexOf(img) == n -1) {
-        document.getElementById("moveR").style.opacity = ".2";
-        document.getElementById("moveR").classList.remove("Pointer");
-        document.getElementById("moveL").style.opacity = "1";
-        document.getElementById("moveL").classList.add("Pointer");
-        Position = "last";
-    }
-    else {
-        document.getElementById("moveL").style.opacity = "1";
-        document.getElementById("moveL").classList.add("Pointer");
-        document.getElementById("moveR").style.opacity = "1";
-        document.getElementById("moveR").classList.add("Pointer");
-        Position = "";
-    }
-}
+// function OpenImg(Sezione, Cantiere, n, img) {
+//     Curr = img;
+//     len = n - 1;
+//     ToChange.style.backgroundImage = 'url(../../../../Images/SV/' + Sezione + '/' + Cantiere + '/' + img + '.jpg)';
+//     // if (img == "l" || img == "k" || img == "m" & Sezione == "Coperture" & Cantiere == "Siracusa") {
+//     //     ToChange.style.backgroundImage = 'url(../../../../Images/SV/' + Sezione + '/' + Cantiere + '/' + img + '.jpg)';
+//     // }
+//     // else {
+//     //     ToChange.style.backgroundImage = 'url(../../../../Images/SV/' + Sezione + '/' + Cantiere + '/' + img + '.JPG)';
+//     // }
+//     ImgZoom.style.display = "block";
+//     let h = document.querySelector("html");
+//     h.style.overflowY = "hidden";
+//     if (abc.indexOf(img) == 0) {
+//         document.getElementById("moveL").style.opacity = ".2";
+//         document.getElementById("moveL").classList.remove("Pointer");
+//         document.getElementById("moveR").style.opacity = "1";
+//         document.getElementById("moveR").classList.add("Pointer");
+//         Position = "first";
+//     }
+//     else if (abc.indexOf(img) == n -1) {
+//         document.getElementById("moveR").style.opacity = ".2";
+//         document.getElementById("moveR").classList.remove("Pointer");
+//         document.getElementById("moveL").style.opacity = "1";
+//         document.getElementById("moveL").classList.add("Pointer");
+//         Position = "last";
+//     }
+//     else {
+//         document.getElementById("moveL").style.opacity = "1";
+//         document.getElementById("moveL").classList.add("Pointer");
+//         document.getElementById("moveR").style.opacity = "1";
+//         document.getElementById("moveR").classList.add("Pointer");
+//         Position = "";
+//     }
+// }
 
-function MoveL() {
-    if (Position != "first") {
-        ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) - 1] + '.jpg")';
-        // if (abc[abc.indexOf(Curr) - 1] == "l") {
-        //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) - 1] + '.jpg")';
-        // }
-        // else {
-        //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) - 1] + '.JPG")';
-        // }
-        if (Curr == "b") {
-            document.getElementById("moveL").style.opacity = ".2";
-            document.getElementById("moveL").classList.remove("Pointer");
-            Position = "first";
-        }
-        else {
-            Position = "";
-        }
-        Curr = abc[abc.indexOf(Curr) - 1];
-        document.getElementById("moveR").style.opacity = "1";
-        document.getElementById("moveR").classList.add("Pointer");
-    }
- }
+// function MoveL() {
+//     if (Position != "first") {
+//         ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) - 1] + '.jpg")';
+//         // if (abc[abc.indexOf(Curr) - 1] == "l") {
+//         //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) - 1] + '.jpg")';
+//         // }
+//         // else {
+//         //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) - 1] + '.JPG")';
+//         // }
+//         if (Curr == "b") {
+//             document.getElementById("moveL").style.opacity = ".2";
+//             document.getElementById("moveL").classList.remove("Pointer");
+//             Position = "first";
+//         }
+//         else {
+//             Position = "";
+//         }
+//         Curr = abc[abc.indexOf(Curr) - 1];
+//         document.getElementById("moveR").style.opacity = "1";
+//         document.getElementById("moveR").classList.add("Pointer");
+//     }
+//  }
 
-function MoveR() {
-    if (Position != "last") {
-        ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) + 1] + '.jpg")';
-        // if (abc[abc.indexOf(Curr) + 1] == "l" || abc[abc.indexOf(Curr) + 1] == "k" || abc[abc.indexOf(Curr) + 1] == "m") {
-        //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) + 1] + '.jpg")';
-        // }
-        // else {
-        //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) + 1] + '.JPG")';
-        // }
-        if (Curr == abc[len - 1]) {
-            document.getElementById("moveR").style.opacity = ".2";
-            document.getElementById("moveR").classList.remove("Pointer");
-            Position = "last";
-        }
-        else {
-            Position = "";
-        }
-        Curr = abc[abc.indexOf(Curr) + 1];
-        document.getElementById("moveL").style.opacity = "1";
-        document.getElementById("moveL").classList.add("Pointer");
-    }
-}
+// function MoveR() {
+//     if (Position != "last") {
+//         ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) + 1] + '.jpg")';
+//         // if (abc[abc.indexOf(Curr) + 1] == "l" || abc[abc.indexOf(Curr) + 1] == "k" || abc[abc.indexOf(Curr) + 1] == "m") {
+//         //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) + 1] + '.jpg")';
+//         // }
+//         // else {
+//         //     ToChange.style.backgroundImage = ToChange.style.backgroundImage.slice(0, -7) + abc[abc.indexOf(Curr) + 1] + '.JPG")';
+//         // }
+//         if (Curr == abc[len - 1]) {
+//             document.getElementById("moveR").style.opacity = ".2";
+//             document.getElementById("moveR").classList.remove("Pointer");
+//             Position = "last";
+//         }
+//         else {
+//             Position = "";
+//         }
+//         Curr = abc[abc.indexOf(Curr) + 1];
+//         document.getElementById("moveL").style.opacity = "1";
+//         document.getElementById("moveL").classList.add("Pointer");
+//     }
+// }
 
-function CloseImg() {
-    ImgZoom.style.display = "none";
-    h.style.overflowY = "scroll";
-}
+// function CloseImg() {
+//     ImgZoom.style.display = "none";
+//     h.style.overflowY = "scroll";
+// }
