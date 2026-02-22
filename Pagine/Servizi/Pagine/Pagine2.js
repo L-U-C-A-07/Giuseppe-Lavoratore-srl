3ae5130c0b2c146e096b13c35f719dcc401fda67
L-U-C-A <lucaalbertodalboni2@gmail.com>
2026-02-22 12:49:57 +0100

    v 0.5.7
    finito linee vita

--- Pagine/Servizi/Pagine/Pagine2.js
+++ Pagine/Servizi/Pagine/Pagine2.js
@@ -7,28 +7,40 @@ var sez
 var cant
 
 function OpenImg(Sezione, Cantiere, n) {
-    Curr = 1;
-    len = n;
-    sez = Sezione
-    cant = Cantiere
-    let pathBase = `../../../../Images/SV/${sez}/${cant}/${Curr}`;
-    let img = new Image();
-    img.onload = function() {
-        // Se la trova con .jpg, la applica
-        ToChange.style.backgroundImage = `url(${pathBase}.jpg)`;
-    };
-    img.onerror = function() {
-        // Se .jpg fallisce, prova con .JPG
-        ToChange.style.backgroundImage = `url(${pathBase}.JPG)`;
-    };
-    img.src = `${pathBase}.jpg`; // Avvia il primo tentativo
-    document.getElementById("moveL").style.opacity = ".2";
-    document.getElementById("moveL").classList.remove("Pointer");
-    document.getElementById("moveR").style.opacity = "1";
-    document.getElementById("moveR").classList.add("Pointer");
-    ImgZoom.style.display = "block";
-    let h = document.querySelector("html");
-    h.style.overflowY = "hidden";
+    if (Sezione != "") {
+        Curr = 1;
+        len = n;
+        sez = Sezione
+        cant = Cantiere
+        let pathBase = `../../../../Images/SV/${sez}/${cant}/${Curr}`;
+        let img = new Image();
+        img.onload = function() {
+            // Se la trova con .jpg, la applica
+            ToChange.style.backgroundImage = `url(${pathBase}.jpg)`;
+        };
+        img.onerror = function() {
+            // Se .jpg fallisce, prova con .JPG
+            ToChange.style.backgroundImage = `url(${pathBase}.JPG)`;
+        };
+        img.src = `${pathBase}.jpg`; // Avvia il primo tentativo
+        document.getElementById("moveL").style.display = "flex";
+        document.getElementById("moveL").style.opacity = ".2";
+        document.getElementById("moveL").classList.remove("Pointer");
+        document.getElementById("moveR").style.display = "flex";
+        document.getElementById("moveR").style.opacity = "1";
+        document.getElementById("moveR").classList.add("Pointer");
+        ImgZoom.style.display = "block";
+        let h = document.querySelector("html");
+        h.style.overflowY = "hidden";
+    }
+    else {
+        document.getElementById("moveL").style.display = "none";
+        document.getElementById("moveR").style.display = "none";
+        ToChange.style.backgroundImage = `url(${Cantiere})`;
+        ImgZoom.style.display = "block";
+        let h = document.querySelector("html");
+        h.style.overflowY = "hidden";
+    }
 }
 
 function MoveL() {
